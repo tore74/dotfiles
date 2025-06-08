f30246508f68a4468538a5a4961b4daeb118b34f
tore <tore74@gmail.com>
2025-06-09 00:42:54 +0200

    Commit 2

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -1,3 +1,45 @@
--- Map jj to esc
---map('i', 'jj', '<esc>l', defaults)
+-- keymaps.lua
+--
+-- Function Map makes remapping easier:
+-- This code just maps vim.keymap.set() to something 
+-- easier to type. 
+-- Also, typing { noremap = true, silent = true } won’t 
+-- be needed, because that is also implemented in the function.
+function Map(mode, lhs, rhs, opts)
+    local options = { noremap = true, silent = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.keymap.set(mode, lhs, rhs, options)
+end
+
+vim.g.mapleader = " "
+
+-- Navigate splits
+Map("n", "<C-h>", "<C-w>h")
+Map("n", "<C-j>", "<C-w>j")
+Map("n", "<C-k>", "<C-w>k")
+Map("n", "<C-l>", "<C-w>l")
+-- terminal
+Map("t", "<C-h>", "<cmd>wincmd h<CR>")
+Map("t", "<C-j>", "<cmd>wincmd j<CR>")
+Map("t", "<C-k>", "<cmd>wincmd k<CR>")
+Map("t", "<C-l>", "<cmd>wincmd l<CR>")
+
+-- Resize splits
+Map("n", "<C-Up>", ":resize -2<CR>")
+Map("n", "<C-Down>", ":resize +2<CR>")
+Map("n", "<C-Left>", ":vertical resize -2<CR>")
+Map("n", "<C-Right>", ":vertical resize +2<CR>")
+-- terminal
+Map("t", "<C-Up>", "<cmd>resize -2<CR>")
+Map("t", "<C-Down>", "<cmd>resize +2<CR>")
+Map("t", "<C-Left>", "<cmd>vertical resize -2<CR>")
+Map("t", "<C-Right>", "<cmd>vertical resize +2<CR>")
+
+-- Navigate buffers with Tab
+Map("n", "<TAB>", ":bn<CR>")
+Map("n", "<S-TAB>", ":bp<CR>")
+Map("n", "<leader>bd", ":bd<CR>") -- from Doom Emacs
+
 
